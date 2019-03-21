2a2b587f5c4e3d30464cffbf96ec68b5c0660279
Daniel De Neef <daniel.deneef@gmail.com>
2019-03-21 21:09:16 +0100

    finish the Colors Enum with adding the colors + the colors code into the array for R G B

--- src/enums/Colors.java
+++ src/enums/Colors.java
@@ -1,4 +1,18 @@
 package enums;
 
 public enum Colors {
+
+    BLACK(0,0,0),
+    WHITE(255,255,255),
+    RED(255,0,0),
+    GREEN(0,255,0),
+    BLUE(0,0,255);
+
+    private int [] rgb=new int[3];
+
+    Colors(int r,int g,int b) {
+        rgb[0]=r;
+        rgb[1]=g;
+        rgb[2]=b;
+    }
 }
