684fd439922918801d1298a56999ee02615b079b
Daniel De Neef <daniel.deneef@gmail.com>
2019-03-21 22:21:21 +0100

    finish the abstract boat class

--- src/boats/Boat.java
+++ src/boats/Boat.java
@@ -1,4 +1,42 @@
 package boats;
 
-public class Boat {
+import interfaces.Floatable;
+
+public abstract class Boat implements Floatable {
+
+    private static int count;
+    private String city;
+    private int number;
+    private String callSign;
+    private double speed;
+
+    public Boat(){
+
+    }
+
+    public Boat(String city,int number){
+        this.city=city;
+        this.number=number;
+        this.callSign=city+number;
+    }
+
+    public void setCity(String city){
+        this.city=city;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public void setNumber(int number){
+        this.number=number;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+    public int getCount(){
+        return count;
+    }
+
 }
